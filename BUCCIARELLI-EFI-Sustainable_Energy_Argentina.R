6f9ec2941258b65c47508f5d7764a913e6836337
nazabucciarelli <bnazareno03@gmail.com>
2023-10-17 17:33:47 -0300

    work may be finished

--- BUCCIARELLI-EFI-Sustainable_Energy_Argentina.R
+++ BUCCIARELLI-EFI-Sustainable_Energy_Argentina.R
@@ -1,9 +1,15 @@
-sustainable_energy_db <- read.csv("ITEC/Matematica Aplicada/efi/sustainable_energy_argentina.csv")
+sustainable_energy_db <- read.csv("sustainable_energy_argentina.csv")
 summary_db <- summary(sustainable_energy_db)
 
-anio_prediccion = 2050
+separador <- function(arg) {
+  cat(paste("-------",arg,"-------\n"))
+  cat("\n")
+}
 
-# ---------------------------- Modelo de Predicción para la Salida de Electricidad Renovable por año en Argentina -------------------------------------------
+anio_prediccion = 2030  # Modificar este valor para obtener distintos resultados.
+
+# ----------------------------Modelo de Predicción para la Salida de Electricidad Renovable por año en Argentina (GWh)------------------
+separador("Modelo de Predicción para la Salida de Electricidad Renovable por año en Argentina (GWh)")
 
 # Genero regresion lineal 
 
@@ -26,6 +32,7 @@ salida_electricidad_renovable = as.double(predict(regresion_salida_elec_renovabl
 salida_electricidad_renovable
 
 # ---------------------------- Modelo de Predicción para el Consumo de Energia Renovable por año en Argentina (Terajoule-TJ)----------------------------------
+separador("Modelo de Predicción para el Consumo de Energia Renovable por año en Argentina (Terajoule-TJ)")
 
 # Genero regresion lineal 
 
@@ -46,6 +53,7 @@ datos_prediccion_consumo_ER <- data.frame(TIME=anio_prediccion,REN.ELECTRICITY.O
 cons_energia_renovable = as.double(predict(regresion_consumo_ER_ajustado, datos_prediccion_consumo_ER))
 cons_energia_renovable
 # ---------------------------- Modelo de Predicción para la Salida Total de Electricidad por año en Argentina (GWh) ------------------------------------------------
+separador("Modelo de Predicción para la Salida Total de Electricidad por año en Argentina (GWh)")
 
 # Genero regresion lineal 
 
@@ -67,6 +75,7 @@ salida_total_electricidad = as.double(predict(regresion_salida_elec_total_ajusta
 salida_total_electricidad
 
 # ---------------------------- Modelo de Predicción para el Total Final de Consumo de Energia por año en Argentina (Terajoules - TJ) ------------------------------------------------------
+separador("Modelo de Predicción para el Total Final de Consumo de Energia por año en Argentina (Terajoules - TJ)")
 
 # Genero regresion lineal 
 
@@ -88,24 +97,27 @@ total_final_consumo_energia = as.double(predict(regresion_total_cons_energia_aju
 total_final_consumo_energia
 
 # ---------------------------- Modelo de Predicción para el Nivel de Intensidad de Energia Primaria por año en Argentina (GWh) ---------------------------------------------------------
+separador("Modelo de Predicción para el Nivel de Intensidad de Energia Primaria por año en Argentina (GWh)")
 # Este modelo sirve para medir la eficiencia de la energía.
 
 # Genero regresion lineal 
 
-regresion_intensidad_energ_prim <- lm(PRIMARY.ENERGY.INTENSITY ~ TIME + REN.ELECTRICITY.OUTPUT +  TOTAL.ELECTRICITY.OUTPUT +
-                                   RE.CONSUMPTION +  + TOTAL.FINAL.ENERGY.CONSUM, data=sustainable_energy_db) 
+regresion_intensidad_energ_prim <- lm(PRIMARY.ENERGY.INTENSITY ~ TIME + TOTAL.FINAL.ENERGY.CONSUM +  REN.ELECTRICITY.OUTPUT +  TOTAL.ELECTRICITY.OUTPUT +
+                                        RE.CONSUMPTION , data=sustainable_energy_db) 
 summary(regresion_intensidad_energ_prim)
 
 # Ajusto la regresion para quitar variables no relevantes
 
-regresion_intensidad_energ_prim_ajustado <- lm(PRIMARY.ENERGY.INTENSITY ~ TIME + TOTAL.FINAL.ENERGY.CONSUM, data=sustainable_energy_db) 
+regresion_intensidad_energ_prim_ajustado <- lm(PRIMARY.ENERGY.INTENSITY ~ TIME + REN.ELECTRICITY.OUTPUT +  TOTAL.ELECTRICITY.OUTPUT +
+                                                 TOTAL.FINAL.ENERGY.CONSUM, data=sustainable_energy_db) 
 summary(regresion_intensidad_energ_prim_ajustado)
 
-# Este modelo tiene un 57% de efectividad.
+# Este modelo tiene un 61% de efectividad.
 
 # Construcción del modelo de predicción
 
-datos_prediccion_intensidad_energ_prim_ajustado <- data.frame(TIME=anio_prediccion,TOTAL.FINAL.ENERGY.CONSUM=total_final_consumo_energia)
+datos_prediccion_intensidad_energ_prim_ajustado <- data.frame(TIME=anio_prediccion,TOTAL.FINAL.ENERGY.CONSUM=total_final_consumo_energia,
+                                                              REN.ELECTRICITY.OUTPUT=salida_electricidad_renovable,TOTAL.ELECTRICITY.OUTPUT=salida_total_electricidad)
 nivel_intensidad_ener_primaria = as.double(predict(regresion_intensidad_energ_prim_ajustado, datos_prediccion_intensidad_energ_prim_ajustado))
 nivel_intensidad_ener_primaria
 
@@ -113,7 +125,7 @@ nivel_intensidad_ener_primaria
 # Construyendo matriz de resultado
 
 matriz_resultado <- matrix(c(nivel_intensidad_ener_primaria,salida_electricidad_renovable,cons_energia_renovable,salida_total_electricidad,total_final_consumo_energia),nrow=1,ncol=5)
-colnames(matriz_resultado) <- c("Nivel de Intensidad de Energía Primaria |","Salida de Electricidad Renovable |",
-                                "Consumo de Energía Renovable |","Salida Total de Electricidad |","Total Final de Consumo de Energía |")
+colnames(matriz_resultado) <- c("Niv. de Intens. de Energía Primaria (GWh)|","Salida de Elect. Renovable (GWh) |",
+                                "Cons. de Energía Renovable (TJ)|","Salida Tot. de Elect. (GWh)|","Tot. Final de Cons. de Energía (TJ)|")
 rownames(matriz_resultado) <- paste("Año",anio_prediccion)
 matriz_resultado
